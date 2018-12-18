d5bb04f19cf782a10f619332a0520b2978a3b407
ijiangpeiyong <ijiangpeiyong@126.com>
2018-12-18 11:07:31 +0800

    1218

--- DQN/1217/myDQN.py
+++ DQN/1217/myDQN.py
@@ -1,4 +1,301 @@
 # Author : Peiyong Jiang
+# jiangpeiyong@impcas.ac.cn
+#
+
+import os
+import time
+from multiprocessing import Process
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+class DQN:
+    def __init__(self):
+        # ------- 环境 ---------------
+        self.actionSpace = [0, 1, 2, 3]   # 上，下，左，右
+        self.numAction = len(self.actionSpace)
+        self.numCol = 7
+        self.numRow = 5
+        self.barrier = [7, 9, 10, 11, 16]
+        self.girl = [17]
+        self.boy = 0
+
+        self.timeFresh = 0.005
+
+        self.numRun = 300
+        self.counterRun = 0
+
+        self.printName = 'env'
+
+        # --------- 大脑 ------------
+
+        self.numFeature=2
+
+        self.factorGreedyEpsilon=0.5
+        self.factorGreedyEpsilonInc=0.001
+        self.factorGreedyEpsilonMax=1.
+
+        self.factorRewardDecayGamma=0.9
+
+        self.factorLearningRate=0.0001
+
+        self.sizeMemory=10000
+        self.sizeBatch=128
+
+        self.numAssignTE=500
+
+        self.outputNNGraph=True
+
+        self.memory=np.zeros((self.sizeMemory,self.numFeature*2+2))
+
+        self.counterMemory=0
+        self.counterLearn=0
+
+        self.histLoss=[]
+
+        # ------- 初始化 ： 环境 ---------------
+        self.BuildEnv()
+
+        # ------- 初始化 ： 大脑 ---------------
+
+        self.BuildNet()
+
+
+        # Get all variables in the netTarget and netEval
+        paramsTarget=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope='netTarget')
+        paramsEval=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope='netEval')
+
+        with tf.variable_scope('assignTE'):
+            self.assignTE=[tf.assign(t,e) for t,e in zip(paramsTarget,paramsEval)]
+
+        self.sess=tf.Session()
+        if self.outputNNGraph:
+            os.system('rm -fr ./logs/*')
+            tf.summary.FileWriter("logs/",self.sess.graph)
+
+        self.sess.run(tf.global_variables_initializer())        
+
+
+
+    def BuildEnv(self):
+        self.env = [0]*(self.numCol*self.numRow)
+        for iBarrier in self.barrier:
+            self.env[iBarrier] = -1
+        for iGirl in self.girl:
+            self.env[iGirl] = 1
+
+    def Print(self, pltTitle='self.counterRun'):
+        plt.figure(self.printName)
+
+        plt.clf()
+
+        iRow, iCol = 0, 0
+        for iEnv in self.env:
+            if iEnv == 0:
+                plt.plot(iCol, iRow, 'ys', markersize='40')
+            elif iEnv == -1:
+                plt.plot(iCol, iRow, 'ks', markersize='40')
+            elif iEnv == 1:
+                plt.plot(iCol, iRow, 'rs', markersize='40')
+
+            iCol += 1
+            if iCol == self.numCol:
+                iRow += 1
+                iCol = 0
+
+        iRow = self.boy // self.numCol
+        iCol = self.boy % self.numCol
+        plt.plot(iCol, iRow, 'bo', markersize='30')
+
+        plt.axis('equal')
+        plt.axis([-1, 7, -1, 5])
+
+        if pltTitle == 'self.counterRun':
+            pltTitle = self.counterRun
+        plt.title(pltTitle)
+
+        plt.pause(self.timeFresh)
+
+        # plt.show()
+
+    def Reset(self):
+        self.boy = 0
+        self.counterRun = 0
+        self.Print()
+
+    def UpdateState(self, stateNow, actionNow):
+
+        numState = self.numCol*self.numRow
+        stateNext = stateNow
+
+        if actionNow == 0:    # 下
+            if stateNext >= self.numCol:
+                stateNext -= self.numCol
+                # print('下')
+        if actionNow == 1:      # 上
+            if stateNext < numState-self.numCol:
+                stateNext += self.numCol
+                # print('上')
+        if actionNow == 2:    # 左
+            if (stateNext % self.numCol) > 0:
+                stateNext -= 1
+                # print('左')
+        if actionNow == 3:     # 右
+            if (stateNext % self.numCol) < self.numCol-1:
+                stateNext += 1
+                # print('右')
+
+        if stateNext in self.barrier:
+            rewardNow = -1
+            doneNow = True
+
+        elif stateNext in self.girl:
+            rewardNow = 1
+            doneNow = True
+        else:
+            rewardNow = 0
+            doneNow = False
+
+        if self.counterRun == self.numRun:
+            rewardNow += 0
+            doneNow = True
+
+        rewardNow += 0.003
+
+        self.boy = stateNext
+
+        self.Print()
+
+        self.counterRun += 1
+
+        counterRun = self.counterRun
+
+        return stateNext, rewardNow, doneNow, counterRun
+
+    def GetState(self):
+        return self.boy, self.counterRun
+
+    def SetState(self, state):
+        self.boy = state
+
+
+    def BuildNet(self):
+        # 整体思路：
+
+        # 输入
+        self.stateNow=tf.placeholder(tf.float32,[None,self.numFeature],name='stateNow')
+        self.stateNext=tf.placeholder(tf.float32,[None,self.numFeature],name='stateNext')
+        self.rewardNow=tf.placeholder(tf.float32,[None,],name='rewardNow')
+        self.actionNow=tf.placeholder(tf.int32,[None,],name='actionNow')
+
+        # 初始化
+        initializeW,initializeB=tf.random_normal_initializer(0,0.3),tf.constant_initializer(0.1)
+
+
+        # build evaluate net:
+        with tf.variable_scope('netEval'):
+            netEval_1=tf.layers.dense(self.stateNow,20,tf.nn.relu,
+            kernel_initializer=initializeW,bias_initializer=initializeB,name='netEval_1')
+
+            self.netEval=tf.layers.dense(netEval_1,self.numAction,
+            kernel_initializer=initializeW,bias_initializer=initializeB,name='netEval')
+
+        # Build target net:
+        with tf.variable_scope('netTarget'):
+            netTarget_1=tf.layers.dense(self.stateNext,20,tf.nn.relu,
+            kernel_initializer=initializeW,bias_initializer=initializeB,name='netTarget_1')
+
+            self.netTarget=tf.layers.dense(netTarget_1,self.numAction,
+            kernel_initializer=initializeW,bias_initializer=initializeB,name='netTarget')
+
+        # qTarget：未来的
+        # qTarget = r + gamma * qMaxS_
+        with tf.variable_scope('qTarget'):
+            qTarget=self.rewardNow+self.factorRewardDecayGamma*tf.reduce_max(self.netTarget,axis=1,name='qTarget')
+            self.qTarget=tf.stop_gradient(qTarget)
+
+
+        # qEval: 现在的
+        with tf.variable_scope('qEval'):
+            indexAction=tf.stack([tf.range(tf.shape(self.actionNow)[0],dtype=tf.int32),self.actionNow],axis=1)
+            self.qEval=tf.gather_nd(params=self.netEval,indices=indexAction)
+
+        # loss
+        with tf.variable_scope('loss'):
+            self.loss=tf.reduce_mean(tf.squared_difference(self.qTarget,self.qEval,name='TD_Error'),name='loss')
+
+        # train:
+        with tf.variable_scope('train'):
+            self.train=tf.train.RMSPropOptimizer(self.factorLearningRate).minimize(self.loss)
+
+
+    def StoreMemory(self,stateNow,counterRunNow,actionNow,rewardNow,stateNext,counterRunNext):
+        pieceMemory=np.hstack((stateNow,counterRunNow,actionNow,rewardNow,stateNext,counterRunNext))
+        indexMemory=self.counterMemory % self.sizeMemory
+        self.memory[indexMemory,:]=pieceMemory
+        self.counterMemory+=1
+
+    def SelSamples(self):
+        if self.counterMemory>self.sizeMemory:
+            indexSample=np.random.choice(self.sizeMemory,size=self.sizeBatch)
+        else:
+            indexSample=np.random.choice(self.counterMemory,size=self.sizeBatch)
+
+        memoryBatch=self.memory[indexSample,:]
+        return memoryBatch
+
+    def SelAction(self,stateNow,counterRunNow):
+        stateNow=np.array([stateNow,counterRunNow])
+        stateNow=stateNow[np.newaxis,:]
+
+        if np.random.uniform()<self.factorGreedyEpsilon:
+            qActionNow=self.sess.run(self.netEval,feed_dict={self.stateNow:stateNow})
+            actionNow=np.argmax(qActionNow)
+            #print('贪婪')
+        else:
+            actionNow=np.random.randint(0,self.numAction)
+            #print('随机')
+
+        return actionNow
+
+    def Learn(self):
+        if self.counterLearn % self.numAssignTE==0:
+            #print('AssignTE')
+            self.sess.run(self.assignTE)
+
+        memoryBatch=self.SelSamples()
+
+        _,lossNow=self.sess.run([self.train,self.loss],feed_dict={
+        self.stateNow:memoryBatch[:,:self.numFeature],
+        self.actionNow:memoryBatch[:,self.numFeature],
+        self.rewardNow:memoryBatch[:,self.numFeature+1],
+        self.stateNext:memoryBatch[:,-self.numFeature:],
+        })
+
+        self.histLoss.append(lossNow)
+
+        self.factorGreedyEpsilon=self.factorGreedyEpsilon+self.factorGreedyEpsilonInc if self.factorGreedyEpsilon<self.factorGreedyEpsilonMax else self.factorGreedyEpsilonMax
+
+        self.counterLearn+=1
+
+    def PlotLoss(self):
+        plt.figure('loss')
+        plt.clf()
+        #print(self.histLoss)
+        #plt.plot(np.arange(len(self.histLoss)),self.histLoss)
+        numPrint=100
+        if len(self.histLoss)>numPrint:
+            plt.plot(self.histLoss[-numPrint:])
+        else:
+            plt.plot(self.histLoss)
+        plt.ylabel('loss')
+        plt.xlabel('training step')
+        plt.title(self.factorGreedyEpsilon)
+        plt.pause(0.001)
+        #plt.show()
 
 
 
@@ -9,4 +306,50 @@
 
 
 
+if __name__ == '__main__':
 
+    myDQN = DQN()
+
+
+    numEpisode=1000000
+
+    counterStep=0
+    iEpisode=0
+    numPreStoreMemory=10
+
+    myDQN.Reset()
+
+    iTest=0
+    while True:
+        stateNow,counterRunNow=myDQN.GetState()
+        actionNow=brain.SelAction(stateNow,counterRunNow)
+
+        stateNext,rewardNow, doneNow,counterRunNow=env.UpdateState(stateNow,actionNow)
+    
+        #print(stateNow,actionNow,stateNext)
+    
+        counterRunNext=counterRunNow+1
+
+        brain.StoreMemory(stateNow,counterRunNow,actionNow,rewardNow,stateNext,counterRunNext)
+
+        if counterStep > numPreStoreMemory and (counterStep % 5==0):
+            brain.Learn()
+
+        counterStep+=1
+
+        if doneNow:
+            env.Reset()
+            stateNext,counterRunNext=env.GetState()
+
+            iEpisode+=1
+            #print(iEpisode,brain.factorGreedyEpsilon)
+            if iEpisode>numEpisode:
+                break
+
+        env.SetState(stateNext)
+
+        brain.PlotLoss()
+
+
+
+    print('END')
